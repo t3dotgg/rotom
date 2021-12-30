fda5b3d86021702d78371a782a1355b7dadd8492
theo <me@t3.gg>
2021-12-29 18:51:19 -0800

    fix build

--- packages/rotom-server/src/routers/post.ts
+++ packages/rotom-server/src/routers/post.ts
@@ -1,14 +1,20 @@
 import { createRouter } from "../createRouter";
 import { z } from "zod";
 
-export const postRouter = createRouter().mutation("add", {
-  input: z.object({
-    id: z.string().uuid().optional(),
-    title: z.string().min(1).max(32),
-    text: z.string().min(1),
-  }),
-  async resolve({ ctx, input }) {
-    console.log("new todo?", input);
-    return input;
-  },
-});
+export const postRouter = createRouter()
+  .mutation("add", {
+    input: z.object({
+      id: z.string().uuid().optional(),
+      title: z.string().min(1).max(32),
+      text: z.string().min(1),
+    }),
+    async resolve({ ctx, input }) {
+      console.log("new todo?", input);
+      return input;
+    },
+  })
+  .query("hi", {
+    async resolve() {
+      return { message: "test message" };
+    },
+  });
